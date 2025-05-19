c52f2976a8841e5af9b5106a4a5303d5b593572d
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-19 21:04:33 +0530

    Add files via upload

---
+++ modulo.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+   
+    int modulo  =  4 % 2 ;
+    
+    
+    printf ("%d", modulo);
+    return 0;
+}
