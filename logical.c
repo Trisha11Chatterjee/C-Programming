b05f52d6810ed8c3e101b83bc15942ff50765dcf
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-19 21:18:41 +0530

    Add files via upload

---
+++ logical.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+   
+    float logical =  (4.0 == 4.0) && (3 == 3) ;
+    
+    
+    printf ("%f", logical );
+    return 0;
+}
