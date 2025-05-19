74157f205ea309f079a938e7a3b4ecee8593162b
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-19 20:32:43 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    /*
+    this is multiline comment 
+    */
+   int number = 25;
+   char star ='*';
+ 
+   float pi= 3.14;
+   int a= 30;
+   int A=40;
+   
+   int age= 22;
+   int final_price= 100;
+    return 0;
+}
