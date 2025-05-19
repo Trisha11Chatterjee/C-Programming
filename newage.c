a64e6818a795fb43de24cf87f80d665d0c8aa615
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-19 20:57:13 +0530

    Add files via upload

---
+++ newage.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+    int a = 22;
+    int b= a;
+    int c= b*6;
+    int d = 1,e;
+    
+    int oldage= 22;
+    int years = 22;
+    
+    int newage= oldage + years;
+    printf ("%d", newage);
+    return 0;
+}
