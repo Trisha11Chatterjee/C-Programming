7620e17df196347b168b951df191f63bffca9b3f
Trisha11Chatterjee <186191051+Trisha11Chatterjee@users.noreply.github.com>
2025-05-19 21:07:35 +0530

    Add files via upload

---
+++ floatexampleonmultiply.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+   
+    float mult  =  4.0 * 2 ;
+    
+    
+    printf ("%f", mult);
+    return 0;
+}
